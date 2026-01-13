2d0d32b20cb443a15b628d48d4a5ac4d51edf5b8
juraev <muhammadjonjuraev18@gmail.com>
2026-01-13 17:20:02 +0500

    and

--- Domain/Entities/Team.cs
+++ Domain/Entities/Team.cs
@@ -5,7 +5,7 @@ public class Team
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public int HackathonId { get; set; }
-    public DateTime CreatedAt { get; set; } =  DateTime.Now;
+    public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
     
     public Hackathon? Hackathon { get; set; }
     public List<Participant>? Participants { get; set; }
